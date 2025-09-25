e29c52fe0e5794267b031ab08c68eb16dfa9220e
Fabasshir Islam <fi192@live.mdx.ac.uk>
2025-09-25 17:25:09 +0100

    updated

---
+++ js/js-game-project/WEB-app/webgame.js
@@ -0,0 +1,14 @@
+
+//let myFirstJob = 'programmer';
+//console.log(myFirstJob);
+//let myCurrentJob = "teacher";
+//console.log(myCurrentJob);
+let isNotIsland = true;
+let language = "Bengali";
+const country = "Bangladesh";
+let population = 23525463632463463.456;
+console.log(typeof isNotIsland); 
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof population);
+console.log(population);
