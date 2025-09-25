8492084b945672776c62191b79cecd7ac2e72284
Fabasshir Islam <fi192@live.mdx.ac.uk>
2025-09-25 22:02:53 +0100

    update

--- js/js-game-project/WEB-app/webgame.js
+++ js/js-game-project/WEB-app/webgame.js
@@ -3,12 +3,59 @@
 //console.log(myFirstJob);
 //let myCurrentJob = "teacher";
 //console.log(myCurrentJob);
-let isNotIsland = true;
-let language = "Bengali";
-const country = "Bangladesh";
-let population = 23525463632463463.456;
-console.log(typeof isNotIsland); 
-console.log(typeof language);
-console.log(typeof country);
-console.log(typeof population);
-console.log(population);
+//let isNotIsland = true;
+//let language = "Bengali";
+//const country = "Bangladesh";
+//let population = 23525463632463463.456;
+//console.log(typeof isNotIsland); 
+//console.log(typeof language);
+//console.log(typeof country);
+//console.log(typeof population);
+//console.log(population);
+//true;
+//console.log(true);
+//console.log("'did anyone came?'"); let noOneCame = false;
+//console.log(noOneCame);
+//noOneCame = "why do you care?";
+//console.log(noOneCame);
+// console.log(typeof null);//object
+//const baby = 'ayo';
+//console.log(baby);
+//baby = 'ayo3';
+//console.log(baby); //wont show because const cant be changed
+//var and let are quite same as both can be changed and output it but its quite different as well 
+//var honey = 'pie';
+//console.log(honey);
+//honey = 'honey';
+//console.log(honey);
+//const ageMahir = 2025-2004;
+//const ageGonna =2025-2006;
+//console.log(ageMahir , ageGonna);//like this you can show multiple outputs
+//console.log(ageMahir * 2 ,ageGonna / 2,2**3);//you can do operations as well //2**3 means 2 to the power of 3 
+// let agePerson = 50;
+// const ageBoy = agePerson - 30;
+// const ageGirl = agePerson - 31;
+// console.log(ageBoy , ageGirl);
+// ageBoy = 40; //wont show because const cant be changed
+//const firstName = 'Fabasshir';
+//const lastName = 'Islam';
+//console.log(firstName + lastName); //this will show FabasshirIslam concatenation.
+//console.log(firstName + ' ' +lastName);//this will show Fabasshir Islam concatenation with space.
+//let x = 10+5;// is 15
+//x += 10;//means x = x + 10 = 25.
+//x + 20;
+//console.log(x);//will show 25 because x + 20 is not stored anywhere.
+//x += 20;
+//console.log(x);//will show 45 because x = x +20 =45.
+//x *= 4;
+//console.log(x);
+//x++; //means x= x+1.
+//x--;//means x = x-1.
+// let y = 5;
+// const z = 10;
+//console.log(y>z);//will show false because 5 is not  > 10.
+//we have got >, < , >= ,<= ,==,===,!=,!==
+let agePerson = 50;
+const ageBoy = agePerson - 30;
+const ageGirl = agePerson - 31;
+console.log(ageGirl <=18); // gives boolean value
