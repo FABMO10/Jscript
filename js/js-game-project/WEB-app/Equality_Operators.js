f733277ec3201c7cfbda8a7c0bff2849bf025692
Fabasshir Islam <fi192@live.mdx.ac.uk>
2025-09-29 20:59:23 +0100

    updated

--- js/js-game-project/WEB-app/Equality_Operators.js
+++ js/js-game-project/WEB-app/Equality_Operators.js
@@ -4,4 +4,18 @@
 //here === is a strict equality operator it checks for value and type
 //if ( age == 18)//will do a type coercion after typing loose in console log 
 //console.log(`You just became an adult ${age} (loose)`);//turns string into number 
-prompt("how old are you?")//user input is always a string 
+const favourite = Number(prompt("how old are you?"));//the input is always a string 
+console.log(favourite);
+console.log(typeof favourite);
+if (favourite === 23) //loose operator turns it string into numbers
+{ console.log("you are an adult ");}
+else if (favourite === 7 ){ 
+    console.log("you are an adult of 7");
+}
+else if (favourite === 9 ){ 
+    console.log("you are an adult of 9 ");
+}else{ 
+    console.log("you are not in the range");
+}
+if (favourite !== 23)
+    console.log("why not 23");
