809fa7bedf6f1026495cfe6a3f899f76f98b87b7
Fabasshir Islam <fi192@live.mdx.ac.uk>
2025-09-29 19:06:52 +0100

    updated

---
+++ js/js-game-project/WEB-app/Equality_Operators.js
@@ -0,0 +1,7 @@
+//const age = '18';
+//if ( age === 18)//Dont need a else block because there is only one thing to check 
+//console.log(`You just became an adult ${age} (strict)`);//will not do type coercion
+//here === is a strict equality operator it checks for value and type
+//if ( age == 18)//will do a type coercion after typing loose in console log 
+//console.log(`You just became an adult ${age} (loose)`);//turns string into number 
+prompt("how old are you?")//user input is always a string 
